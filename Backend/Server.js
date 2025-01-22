b17a41033881e49a8b2394826128441fb47259e8
aniket44788 <darkpanda44788@gmail.comgit config --global user.email darkpanda44788@gmail.comgit config --global user.email darkpanda44788@gmail.com>
2025-01-22 10:59:19 +0530

    save

--- Backend/Server.js
+++ Backend/Server.js
@@ -19,8 +19,8 @@ dataBase.connect((err)=>{
 app.post('/Save' , (req,res) => {
     try {
         const { Firstname,Lastname,Username,Email,Password} = req.body
-        const Sql = "Insert into Pixxel(Firstname,Lastname,Username,Email,Password) Values(?,?,?,?,?)"
-        dataBase.query(Sql,[Firstname,Lastname,Username,Email,Password] , (err,result)=>{
+        const insertData = "Insert into Pixxel(Firstname,Lastname,Username,Email,Password) Values(?,?,?,?,?)"
+        dataBase.query(insertData,[Firstname,Lastname,Username,Email,Password] , (err,result)=>{
           res.send("Data Saved Successfully")
         })
     } catch (error) {
@@ -29,12 +29,16 @@ app.post('/Save' , (req,res) => {
 })
 
 app.get('/View',(req,res) => {
-    const Sql = "Select * from Pixxel"
-    dataBase.query(Sql,(err,result) => {
+    const viewData = "Select * from Pixxel"
+    dataBase.query(viewData,(err,result) => {
         res.send(result)
     })
 })
 
+app.patch('/Update',(req,res) =>{
+    const updateData = " update"
+})
+
 app.listen(5000,() => {
-    console.log("Server Run On Port 6000");
+    console.log("Server Run On Port 5000");
 })
