719160a0f298e370ba2c45c0f3db3dd1b7fe6163
aniket44788 <darkpanda44788@gmail.comgit config --global user.email darkpanda44788@gmail.comgit config --global user.email darkpanda44788@gmail.com>
2025-01-28 13:00:38 +0530

    save

--- Frontend/Pixxelu-Demo/src/assets/Components/Register.jsx
+++ Frontend/Pixxelu-Demo/src/assets/Components/Register.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-import { Link } from "react-router-dom";
 
 function Register() {
 
@@ -379,7 +378,7 @@ function Register() {
                   <div class="flex -mx-3">
                     <div class="w-full px-3 mb-5">
                       <button   type="submit" class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">
-                       <Link to="/Userdata">REGISTER NOW</Link> 
+                      REGISTER NOW
                       </button>
                     </div>
                   </div>
